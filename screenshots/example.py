a70c1f2448dc5ac5e1d0a1ecf8c3d9f62336d827
Angel <angelr4a1@gmail.com>
2020-07-01 00:29:22 -0500

    Ejemplo de syntax python

---
+++ screenshots/example.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+class rocket(object):
+
+    """docstring for rocket"""
+
+    def __init__(self, arg):
+        super(rocket, self).__init__()
+        self.arg = arg
+
+    def showcase(arg):
+
+        """
+        Some code to show the syntax
+        # HACK:
+        >>> print('''hello
+        ... world)
+        """
+
+        @decorator(param = 'spam')
+
+        async def coroutine(db:aio_db.DatabaseConnection) -> List[str]:
+            r"""A coroutine"""
+            await logger.log('working\x12with %r', aio_db)
+            async with db.transatiction():
+                print('Result: {result!r}'.format(result=
+                await db.query(...)))
+
+        mapping = None        # type: Dict[int, Any]  # PEP 484
+
+
+        # a regular expression
+
+
+        get_regex = lambda: re.compile(     # type: ignore
+            r"""\A
+                word
+                (?:                         # a comment
+                    (?P<fill>.)?
+                    (?P<align>[<>=^])       (?# another comment)
+                )?
+                another word\.\.\.
+                (?:\.(?P<precision>0|(?!0) \d+))?
+            \Z""",
+            re.VERBOSE | re.DOTALL)
+
+        # NOTE: Numbers with leading zeroes are invalid in Python3
+        #use 0o...
+
+        answer = func(0xdeadbeef + 0b0010001 + 0123 +0o123 +
+
+                        #complex Numbers
+                        .10e12 + 2j) @ mat
+
+        return R'''No escapes "\" in this \one'''
+
+
+# In[34]:
+
+A = (0,1,2,3)
+
+B = ["a","b","c"]
+
+D = {'a':0,'b':1,'c':2}
+
+
+# In[35]:
+
+movies = pd.read_csv('http://bit.ly/imdbratings')
+movies.head()
+
+
+if __name__ == '__main__':
+    main()
